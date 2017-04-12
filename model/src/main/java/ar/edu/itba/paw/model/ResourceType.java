d703bbb48135161cda927b1a8e3790661135c4cc
Julian Benitez <jubenitez@itba.edu.ar>
2017-04-12 05:44:48 -0300

    Made ResourceType prettier

--- model/src/main/java/ar/edu/itba/paw/model/ResourceType.java
+++ model/src/main/java/ar/edu/itba/paw/model/ResourceType.java
@@ -1,48 +1,36 @@
 package ar.edu.itba.paw.model;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 /**
  * Created by juanfra on 31/03/17.
  */
 public enum ResourceType {
-    WOOD, IRON, GOLD, MONEY;
+    WOOD(0), IRON(1), GOLD(2), MONEY(3);
+    private int id;
 
-    public String toString() {
-        switch (this) {
-            case WOOD: return "wood";
-            case IRON: return "iron";
-            case GOLD: return "gold";
-            case MONEY: return "money";
-            default: throw new RuntimeException("There is no string for this resource");
-        }
+    private static HashMap<ResourceType,String> names = new HashMap<>();
+    static {
+        names.put(WOOD,"wood");
+        names.put(IRON,"iron");
+        names.put(GOLD,"gold");
+        names.put(MONEY,"money");
     }
 
-    public static ResourceType fromString(String s){
-        switch (s.toLowerCase()) {
-            case "wood": return WOOD;
-            case "iron": return IRON;
-            case "gold": return GOLD;
-            case "money": return MONEY;
-            default: return null;
-        }
+    ResourceType(int id) {
+        this.id = id;
+    }
+
+    public String toString() {
+        return names.get(fromId(id));
     }
 
     public int getId() {
-        switch (this) {
-            case WOOD: return 0;
-            case IRON: return 1;
-            case GOLD: return 2;
-            case MONEY: return 3;
-            default: throw new RuntimeException("There is no id for this resource");
-        }
+        return id;
     }
 
     public static ResourceType fromId(int id){
-        switch (id) {
-            case 0: return WOOD;
-            case 1: return IRON;
-            case 2: return GOLD;
-            case 3: return MONEY;
-            default: return null;
-        }
+        return Arrays.asList(ResourceType.values()).stream().filter((r) -> r.getId() ==id).findAny().get();
     }
 }
