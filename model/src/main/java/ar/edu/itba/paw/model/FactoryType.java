187b636f2b4732c2791d763d1ea75d029897b2c9
Julian Benitez <jubenitez@itba.edu.ar>
2017-04-12 06:59:55 -0300

    Filled with new resources!

--- model/src/main/java/ar/edu/itba/paw/model/FactoryType.java
+++ model/src/main/java/ar/edu/itba/paw/model/FactoryType.java
@@ -1,7 +1,6 @@
 package ar.edu.itba.paw.model;
 
 import ar.edu.itba.paw.model.packages.Implementations.BaseCost;
-import ar.edu.itba.paw.model.packages.Implementations.FactoryCost;
 import ar.edu.itba.paw.model.packages.Implementations.BaseRecipe;
 import ar.edu.itba.paw.model.packages.PackageBuilder;
 import ar.edu.itba.paw.model.packages.PackageType;
@@ -10,17 +9,110 @@ import ar.edu.itba.paw.model.packages.PackageType;
  * Created by juanfra on 31/03/17.
  */
 public enum FactoryType {
-    NOTHINGFORWOOD(0),
-    WOODFORIRON(1),
-    IRONFORGOLD(2),
-    WOODFORGOLD(3);
+    BOILER_BASE(0){
+        public BaseCost getBaseCost(){
+            PackageBuilder<BaseCost> costBuilder = PackageType.BaseCostType.packageBuilder();
+            costBuilder.putItem(ResourceType.PEOPLE,20D);
+            costBuilder.putItem(ResourceType.IRON,500D);
+            costBuilder.putItem(ResourceType.COPPER_CABLE,200D);
+            return costBuilder.buildPackage();
+        }
+        public BaseRecipe getBaseRecipe() {
+            PackageBuilder<BaseRecipe> recipeBuilder = PackageType.BaseRecipeType.packageBuilder();
+            recipeBuilder.putItem(ResourceType.CARDBOARD,-10D);
+            recipeBuilder.putItem(ResourceType.POWER,1D);
+            return recipeBuilder.buildPackage();
+        }},
+
+    METAL_SEPARATOR_BASE(1){
+        public BaseCost getBaseCost(){
+            PackageBuilder<BaseCost> costBuilder = PackageType.BaseCostType.packageBuilder();
+            costBuilder.putItem(ResourceType.PEOPLE,2D);
+            return costBuilder.buildPackage();
+        }
+        public BaseRecipe getBaseRecipe() {
+            PackageBuilder<BaseRecipe> recipeBuilder = PackageType.BaseRecipeType.packageBuilder();
+            recipeBuilder.putItem(ResourceType.METAL_SCRAP,-3D);
+            recipeBuilder.putItem(ResourceType.IRON,0.6D);
+            recipeBuilder.putItem(ResourceType.COPPER,0.4D);
+            return recipeBuilder.buildPackage();
+        }},
+
+    STOCK_INVESTMENT_BASE(2){
+        public BaseCost getBaseCost(){
+            PackageBuilder<BaseCost> costBuilder = PackageType.BaseCostType.packageBuilder();
+            costBuilder.putItem(ResourceType.MONEY,1000D);
+            return costBuilder.buildPackage();
+        }
+        public BaseRecipe getBaseRecipe() {
+            PackageBuilder<BaseRecipe> recipeBuilder = PackageType.BaseRecipeType.packageBuilder();
+            recipeBuilder.putItem(ResourceType.MONEY,1D);
+            return recipeBuilder.buildPackage();
+        }},
+
+    JUNK_COLLECTOR_BASE(3){
+        public BaseCost getBaseCost(){
+            PackageBuilder<BaseCost> costBuilder = PackageType.BaseCostType.packageBuilder();
+            costBuilder.putItem(ResourceType.PEOPLE,5D);
+            return costBuilder.buildPackage();
+        }
+        public BaseRecipe getBaseRecipe() {
+            PackageBuilder<BaseRecipe> recipeBuilder = PackageType.BaseRecipeType.packageBuilder();
+            recipeBuilder.putItem(ResourceType.TIRES,0.2D);
+            recipeBuilder.putItem(ResourceType.METAL_SCRAP,0.6D);
+            recipeBuilder.putItem(ResourceType.CARDBOARD,2D);
+            return recipeBuilder.buildPackage();
+        }},
+
+    PEOPLE_RECRUITING_BASE(4){
+        public BaseCost getBaseCost(){
+            PackageBuilder<BaseCost> costBuilder = PackageType.BaseCostType.packageBuilder();
+            costBuilder.putItem(ResourceType.MONEY,200D);
+            return costBuilder.buildPackage();
+        }
+        public BaseRecipe getBaseRecipe() {
+            PackageBuilder<BaseRecipe> recipeBuilder = PackageType.BaseRecipeType.packageBuilder();
+            recipeBuilder.putItem(ResourceType.PEOPLE,0.3D);
+            return recipeBuilder.buildPackage();
+        }},
+
+    RUBBER_SHREDDER_BASE(5){
+        public BaseCost getBaseCost(){
+            PackageBuilder<BaseCost> costBuilder = PackageType.BaseCostType.packageBuilder();
+            costBuilder.putItem(ResourceType.PEOPLE,2D);
+            costBuilder.putItem(ResourceType.IRON,50D);
+            return costBuilder.buildPackage();
+        }
+        public BaseRecipe getBaseRecipe() {
+            PackageBuilder<BaseRecipe> recipeBuilder = PackageType.BaseRecipeType.packageBuilder();
+            recipeBuilder.putItem(ResourceType.TIRES,-2D);
+            recipeBuilder.putItem(ResourceType.RUBBER,0.8D);
+            return recipeBuilder.buildPackage();
+        }},
+
+    CABLE_MAKER_BASE(6){
+        public BaseCost getBaseCost(){
+            PackageBuilder<BaseCost> costBuilder = PackageType.BaseCostType.packageBuilder();
+            costBuilder.putItem(ResourceType.PEOPLE,5D);
+            costBuilder.putItem(ResourceType.IRON,250D);
+            return costBuilder.buildPackage();
+        }
+        public BaseRecipe getBaseRecipe() {
+            PackageBuilder<BaseRecipe> recipeBuilder = PackageType.BaseRecipeType.packageBuilder();
+            recipeBuilder.putItem(ResourceType.COPPER,-3D);
+            recipeBuilder.putItem(ResourceType.RUBBER,-5D);
+            recipeBuilder.putItem(ResourceType.COPPER_CABLE,0.8D);
+            return recipeBuilder.buildPackage();
+        }},
+    ;
 
     private int id;
     FactoryType(int i){
         id=i;
     }
 
-
+    public abstract BaseCost getBaseCost();
+    public abstract BaseRecipe getBaseRecipe();
     public int getId() {
         return id;
     }
@@ -34,54 +126,54 @@ public enum FactoryType {
         return null;
     }
 
-    public BaseCost getBaseCost(){
-        PackageBuilder<BaseCost> costBuilder = PackageType.BaseCostType.packageBuilder();
-
-        switch (this) {
-            case NOTHINGFORWOOD:
-                costBuilder.putItem(ResourceType.WOOD,10D);
-                break;
-            case WOODFORIRON:
-                costBuilder.putItem(ResourceType.WOOD,100D);
-                break;
-            case IRONFORGOLD:
-                costBuilder.putItem(ResourceType.WOOD,100D);
-                costBuilder.putItem(ResourceType.IRON,100D);
-                break;
-            case WOODFORGOLD:
-                costBuilder.putItem(ResourceType.IRON,100D);
-                costBuilder.putItem(ResourceType.WOOD,100D);
-                costBuilder.putItem(ResourceType.GOLD,1000D)    ;
-                break;
-            default:
-                throw new RuntimeException("There is no cost for this factory");
-        }
-
-        return costBuilder.buildPackage();
-    }
-
-    public BaseRecipe getBaseRecipe() {
-        PackageBuilder<BaseRecipe> recipeBuilder = PackageType.BaseRecipeType.packageBuilder();
-
-        switch (this) {
-            case NOTHINGFORWOOD:
-                recipeBuilder.putItem(ResourceType.WOOD, 3D);
-                break;
-            case WOODFORIRON:
-                recipeBuilder.putItem(ResourceType.WOOD, -3D);
-                recipeBuilder.putItem(ResourceType.IRON, 1D);
-                break;
-            case IRONFORGOLD:
-                recipeBuilder.putItem(ResourceType.IRON, -2D);
-                recipeBuilder.putItem(ResourceType.GOLD, 1D);
-                break;
-            case WOODFORGOLD:
-                recipeBuilder.putItem(ResourceType.WOOD, -10D);
-                recipeBuilder.putItem(ResourceType.GOLD, 1D);
-                break;
-            default:
-                throw new RuntimeException("There is no ResourcePackage for this factory");
-        }
-        return recipeBuilder.buildPackage();
-    }
+//    public BaseCost getBaseCost(){
+//        PackageBuilder<BaseCost> costBuilder = PackageType.BaseCostType.packageBuilder();
+//
+//        switch (this) {
+//            case METAL_OVEN_BASE:
+//                costBuilder.putItem(ResourceType.POWER,10D);
+//                break;
+//            case METAL_SEPARATOR_BASE:
+//                costBuilder.putItem(ResourceType.POWER,100D);
+//                break;
+//            case STOCK_INVESTMENT_BASE:
+//                costBuilder.putItem(ResourceType.POWER,100D);
+//                costBuilder.putItem(ResourceType.PLASTIC,100D);
+//                break;
+//            case JUNK_COLLECTOR_BASE:
+//                costBuilder.putItem(ResourceType.PLASTIC,100D);
+//                costBuilder.putItem(ResourceType.POWER,100D);
+//                costBuilder.putItem(ResourceType.GOLD,1000D)    ;
+//                break;
+//            default:
+//                throw new RuntimeException("There is no cost for this factory");
+//        }
+//
+//        return costBuilder.buildPackage();
+//    }
+//
+//    public BaseRecipe getBaseRecipe() {
+//        PackageBuilder<BaseRecipe> recipeBuilder = PackageType.BaseRecipeType.packageBuilder();
+//
+//        switch (this) {
+//            case NOTHINGFORWOOD:
+//                recipeBuilder.putItem(ResourceType.POWER, 3D);
+//                break;
+//            case METAL_SEPARATOR_BASE:
+//                recipeBuilder.putItem(ResourceType.POWER, -3D);
+//                recipeBuilder.putItem(ResourceType.PLASTIC, 1D);
+//                break;
+//            case STOCK_INVESTMENT_BASE:
+//                recipeBuilder.putItem(ResourceType.PLASTIC, -2D);
+//                recipeBuilder.putItem(ResourceType.GOLD, 1D);
+//                break;
+//            case JUNK_COLLECTOR_BASE:
+//                recipeBuilder.putItem(ResourceType.POWER, -10D);
+//                recipeBuilder.putItem(ResourceType.GOLD, 1D);
+//                break;
+//            default:
+//                throw new RuntimeException("There is no ResourcePackage for this factory");
+//        }
+//        return recipeBuilder.buildPackage();
+//    }
 }
--- model/src/main/java/ar/edu/itba/paw/model/ResourceType.java
+++ model/src/main/java/ar/edu/itba/paw/model/ResourceType.java
@@ -7,23 +7,20 @@ import java.util.HashMap;
  * Created by juanfra on 31/03/17.
  */
 public enum ResourceType {
-    WOOD(0), IRON(1), GOLD(2), MONEY(3);
+    POWER(0,"power"), PLASTIC(1,"plastic"), GOLD(2,"gold"),
+    MONEY(3,"money"), PEOPLE(4,"people"), IRON(5,"iron"),
+    TIRES(6,"tires"), RUBBER(7,"rubber"),METAL_SCRAP(8,"metal scrap"),
+    COPPER(9,"copper"), COPPER_CABLE(10,"copper cable"), CARDBOARD(11,"cardboard");
     private int id;
+    private String name;
 
-    private static HashMap<ResourceType,String> names = new HashMap<>();
-    static {
-        names.put(WOOD,"wood");
-        names.put(IRON,"iron");
-        names.put(GOLD,"gold");
-        names.put(MONEY,"money");
-    }
-
-    ResourceType(int id) {
+    ResourceType(int id,String name) {
         this.id = id;
+        this.name = name;
     }
 
     public String toString() {
-        return names.get(fromId(id));
+        return name;
     }
 
     public int getId() {
