a316dbbfb59e1d8fd2edc0b4b0d82b7a93121aff
Julian Benitez <jubenitez@itba.edu.ar>
2017-04-19 05:15:24 -0300

    Added a script to automagically deploy the app. just run ./deploy.sh and input yor password when requested.

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+rm app.war
+mv -b ./webapp/target/webapp.war app.war
+echo "Enter username"
+read username
+echo "Enter password"
+read -s pass
+filename=app.war
+cmd="echo -ne \"cd web/ \n put $filename \n \" | sshpass -p ooc4Choo sftp -oStrictHostKeyChecking=no paw-2017a-4@10.16.1.110"
+#cmd="ls"
+echo $cmd
+echo -ne "pwd\n put $filename\n" | sshpass -p $pass sftp $username@pampero.itba.edu.ar &&
+echo -ne $cmd | sshpass -p $pass ssh $username@pampero.itba.edu.ar
