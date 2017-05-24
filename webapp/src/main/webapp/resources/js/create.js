659a26dd17cc238a16072c042e6ec50cc0063fc0
Pedro Balaguer <pbalaguer@itba.edu.ar>
2017-05-24 17:57:57 -0300

    Back button added in register

--- webapp/src/main/webapp/resources/js/create.js
+++ webapp/src/main/webapp/resources/js/create.js
@@ -3,3 +3,7 @@
 document.getElementById("register").addEventListener("click", function () {
     document.getElementById("registerForm").submit()
 })
+
+document.getElementById("backBtn").addEventListener("click", function () {
+    window.history.back();
+})
