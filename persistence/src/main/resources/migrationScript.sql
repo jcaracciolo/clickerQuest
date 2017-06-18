ab048de1eb16c5842e69f9b4474a7314bd266d63
jcaracciolo <jcaracciolo@itba.edu.ar>
2017-06-18 02:12:02 -0300

    Add migration script

--- persistence/src/main/resources/migrationScript.sql
+++ persistence/src/main/resources/migrationScript.sql
@@ -6,3 +6,9 @@
 --  ADD FOREIGN KEY (clanId) REFERENCES clans(clanId) ON DELETE SET NULL;
 --
 --  ALTER TABLE
+
+
+ALTER table clans add score DOUBLE PRECISION default 0;
+alter table clans add column wins integer not null default 0;
+alter table clans add column battles integer not null default 0;
+CREATE TABLE clansbattle(clanid INT PRIMARY KEY,versusid INT,initialscore DOUBLE PRECISION NOT NULL, FOREIGN KEY(clanid) REFERENCES clans ON DELETE CASCADE,FOREIGN KEY(versusid) REFERENCES clans ON DELETE SET NULL);
