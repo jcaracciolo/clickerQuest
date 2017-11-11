7254f535422b2a8cdc185de7731fa053bd756415
Pedro Balaguer <pbalaguer@itba.edu.ar>
2017-11-11 18:48:33 -0300

    Right panel almost done and JS model simplifed

--- UI-generator/app/scripts/controllers/mainScreenCtrl.js
+++ UI-generator/app/scripts/controllers/mainScreenCtrl.js
@@ -1,44 +1,57 @@
 define(['clickerQuest'], function(clickerQuest) {
 
-    'use strict';
-    clickerQuest.controller('mainScreenCtrl', function($scope) {
-      $scope.resources = [
-        {name:'Circuits', id:'12'},
-        {name:'Cardboard', id:'11'},
-        {name:'Copper Cable', id:'10'},
-        {name:'Copper', id:'9'},
-        {name:'Metal Scrap', id:'8'},
-        {name:'Rubber', id:'7'},
-        {name:'Tires', id:'6'},
-        {name:'Iron', id:'5'},
-        {name:'People', id:'4'},
-        {name:'Money', id:'3'},
-        {name:'Gold', id:'2'},
-        {name:'Plastic', id:'1'},
-        {name:'Power', id:'0'}
-      ];
+  'use strict';
+  clickerQuest.controller('mainScreenCtrl', function($scope) {
 
-      $scope.activeFactories = [
-        {name:'Stock Investment',
-          id:'0',
-          amount:'XXX',
-          consuming:[ ],
-          producing:[ {name:'Money', id:'3', cant:'987654321'} ]
-        },
-        {name:'People Recruiting Base',
-          id:'1',
-          amount:'XXX',
-          consuming:[ {name:'Circuits', id:'12', cant:'123456789'} ],
-          producing:[ {name:'Money', id:'3', cant:'987654321'} ]
-        },
-        {name:'Junk Collector',
-          id:'2',
-          amount:'XXX',
-          consuming:[ {name:'Gold', id:'2', cant:'123456789'} ],
-          producing:[ {name:'Money', id:'3', cant:'987654321'},
-                      {name:'Power', id:'0', cant:'987654321'} ]
-        }
-      ];
-    });
+    // It's important to respect id and position in array
+    $scope.resources = [
+      {name:'Power', id:'0'},
+      {name:'Plastic', id:'1'},
+      {name:'Gold', id:'2'},
+      {name:'Money', id:'3'},
+      {name:'People', id:'4'},
+      {name:'Iron', id:'5'},
+      {name:'Tires', id:'6'},
+      {name:'Rubber', id:'7'},
+      {name:'Metal Scrap', id:'8'},
+      {name:'Copper', id:'9'},
+      {name:'Copper Cable', id:'10'},
+      {name:'Cardboard', id:'11'},
+      {name:'Circuits', id:'12'}
+    ];
+
+    // It's important to respect id and position in array
+    $scope.factories = [
+      {name:'Stock Investment',
+        id:'0',
+        cantActive:'5',
+        cost: [ {id:'3', cant:'100'},
+                {id:'0', cant:'100'} ],
+        recipe: {input: [ {id:'1', cant:'2'},
+                          {id:'2', cant:'2'} ],
+          output: [ {id:'3', cant:'1'},
+                    {id:'0', cant:'1'} ]},
+        upgrade: {type:'1', cost:'XXXX'}
+      },
+      {name:'People Recruitment Base',
+        id:'1',
+        cantActive:'3',
+        cost: [ {id:'6', cant:'50'} ],
+        recipe: {input: [ {id:'4', cant:'3'},
+                          {id:'1', cant:'1'} ],
+          output: [ {id:'3', cant:'1'} ]},
+        upgrade: {type:'1', cost:'XXXX'}
+      },
+      {name:'Junk Collector',
+        id:'2',
+        cantActive:'0',
+        cost: [ {id:'7', cant:'160'}],
+        recipe: {input: [ ],
+          output: [ {id:'12', cant:'1'} ]},
+        upgrade: {type:'1', cost:'XXXX'}
+      }
+    ];
+
+  });
 
 });
