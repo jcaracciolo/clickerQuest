803feeab9b8ba0abb8eb5a2c7b2a7fc02030c104
Pedro Balaguer <pbalaguer@itba.edu.ar>
2017-11-10 23:51:23 -0300

    Converted left panel to angular

--- UI-generator/app/scripts/controllers/mainScreenCtrl.js
+++ UI-generator/app/scripts/controllers/mainScreenCtrl.js
@@ -2,7 +2,21 @@ define(['clickerQuest'], function(clickerQuest) {
 
     'use strict';
     clickerQuest.controller('mainScreenCtrl', function($scope) {
-        
+      $scope.resources = [
+        {name:'Circuits', id:'12'},
+        {name:'Cardboard', id:'11'},
+        {name:'Copper Cable', id:'10'},
+        {name:'Copper', id:'9'},
+        {name:'Metal Scrap', id:'8'},
+        {name:'Rubber', id:'7'},
+        {name:'Tires', id:'6'},
+        {name:'Iron', id:'5'},
+        {name:'People', id:'4'},
+        {name:'Money', id:'3'},
+        {name:'Gold', id:'2'},
+        {name:'Plastic', id:'1'},
+        {name:'Power', id:'0'}
+      ];
     });
 
 });
