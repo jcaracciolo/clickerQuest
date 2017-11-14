47c8adf48cab41e0c969a3d4d8f89170d3b603c5
Pedro Balaguer <pbalaguer@itba.edu.ar>
2017-11-14 17:18:25 -0300

    Profile and Clan angularized

--- UI-generator/app/scripts/routes.js
+++ UI-generator/app/scripts/routes.js
@@ -11,6 +11,14 @@ define([], function() {
             '/game': {
                 templateUrl: '/views/game/mainScreenCtrl.html',
                 controller: 'mainScreenCtrl'
+            },
+            '/profile': {
+                templateUrl: '/views/profile/profileCtrl.html',
+                controller: 'profileCtrl'
+            },
+            '/clan': {
+                templateUrl: '/views/clan/clanCtrl.html',
+                controller: 'clanCtrl'
             }
             /* ===== yeoman hook ===== */
             /* Do not remove these commented lines! Needed for auto-generation */
