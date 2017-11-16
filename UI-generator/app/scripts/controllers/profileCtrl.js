9f5afec6d543c467575f171edcdb2676cbbd5bb8
Pedro Balaguer <pbalaguer@itba.edu.ar>
2017-11-16 20:45:43 -0300

    Profile started linking with API

--- UI-generator/app/scripts/controllers/profileCtrl.js
+++ UI-generator/app/scripts/controllers/profileCtrl.js
@@ -1,7 +1,7 @@
-define(['clickerQuest'], function(clickerQuest) {
+define(['clickerQuest', 'services/UserService', 'services/ClanService'], function(clickerQuest) {
 
     'use strict';
-    clickerQuest.controller('profileCtrl', function($scope) {
+    clickerQuest.controller('profileCtrl', function($scope, UserService, ClanService) {
       $scope.user = {
         username: "Jorgito",
         score: "55.12",
@@ -11,6 +11,27 @@ define(['clickerQuest'], function(clickerQuest) {
                 name: "MY CLAN"
               }
       };
+
+      UserService.getUser(1).then(
+        function (response) {
+          $scope.user.username = response.data.username;
+          $scope.user.score = response.data.score;
+
+          ClanService.getClan(response.data.clan_id).then(
+            function (response) {
+              $scope.user.clan.id = response.data.id;
+              $scope.user.clan.name = response.data.name;
+            }
+          );
+
+          UserService.getRank(response.data.id).then(
+            function (response) {
+              $scope.user.ranking = response.data.rank;
+            }
+          )
+        }
+      );
+
 
       // It's important to respect id and position in array
       $scope.resources = [
@@ -28,6 +49,17 @@ define(['clickerQuest'], function(clickerQuest) {
         {name:'Cardboard', id: 11, stock: 25, production: 50.2},
         {name:'Circuits', id: 12, stock: 25, production: 50.2}
       ];
+
+      UserService.getWealth(1).then(
+        function (response) {
+          $scope.resources = []
+          var storage = response.data.storage
+          var productions = response.data.productions
+          for (var property in storage) {
+            $scope.resources.push({name:property, id: 0, stock: storage[property][0], production: productions[property][0]})
+          }
+        }
+      );
 
       // It's important to respect id and position in array
       $scope.factories = [
